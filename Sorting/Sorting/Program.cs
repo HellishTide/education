40229d1fad7756405275438d11e167d39cbd1a9d
Alexander Smirnov <allexandr.smirnof@gmail.com>
2020-02-02 14:33:17 +0300

    Add bfs

--- Sorting/Sorting/Program.cs
+++ Sorting/Sorting/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Net.Mail;
 
 namespace Sorting
@@ -13,36 +14,14 @@ namespace Sorting
             {
                 array[i] = new Random().Next(10);
                 Console.Write("{0} ", array[i]);
-            }*//*
+            }
             int[] array = { 3, 7, 13, 5, 2, 11, 2, 13, 10 };
             Console.WriteLine();
             sort.QuickSort(array);
             for (int i = 0; i < array.Length; ++i)
                 Console.Write("{0} ", array[i]);
-            */
-
-
-            using (SmtpClient client = new SmtpClient())
-            {
-                try
-                {
-                    MailMessage mail = new MailMessage();
-                    mail.From = new MailAddress("t.t@t.t");
-                    //mail.To.Add(new MailAddress("е@mail.s"));
-                    //throw new SmtpFailedRecipientException();
-
-                    client.Host = "localhost";
-                    client.Port = 25;
-                    //client.EnableSsl = true;
-
-                    client.Send(mail);
-                }
-                catch (Exception e)
-                {
-                    var t = new SmtpFailedRecipientException("bad");
-                    var rx = e;
-                }
-            }
+                */
+                
         }
     }
 }
