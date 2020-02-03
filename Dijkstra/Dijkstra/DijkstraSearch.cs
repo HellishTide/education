9ee593f55bd8fa11052fe7e2f290a84b9f898eee
Alexander Smirnov <allexandr.smirnof@gmail.com>
2020-02-03 18:48:50 +0300

    Create dijkstra project

---
+++ Dijkstra/Dijkstra/DijkstraSearch.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dijkstra
+{
+    public class DijkstraSearch
+    {
+        private Dictionary<string, string> Graph { get; set; } = new Dictionary<string, string>();
+
+    }
+}
