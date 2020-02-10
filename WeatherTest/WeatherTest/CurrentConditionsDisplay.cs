ddc7b28a464d24347ff144a6f5a0ecb806e2d9fe
Alexander Smirnov <allexandr.smirnof@gmail.com>
2020-02-10 18:45:52 +0300

    coding

---
+++ WeatherTest/WeatherTest/CurrentConditionsDisplay.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WeatherTest
+{
+    public class CurrentConditionsDisplay: IObserver<WeatherData>, IDisplayElement
+    {
+        private float temperature;
+        private float humidity;
+        IObservable<T>
+
+        public CurrentConditionsDisplay(ISubject weatherData)
+        {
+            this.weatherData = weatherData;
+            weatherData.RegisterObserver(this);
+        }
+
+        public void Update(float temperature, float humidity, float pressure)
+        {
+            this.temperature = temperature;
+            this.humidity = humidity;
+            Display();
+        }
+
+        public void Display()
+        {
+            Console.WriteLine("Current conditions: {0} F degrees and {1} humidity", temperature, humidity);
+        }
+    }
+}
---
+++ WeatherTest/WeatherTest/WeatherData.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WeatherTest
+{
+    public class WeatherData: IObservable<ISubject>, IObserver
+    {
+        public float Temperature { get; set; }
+        public float Humidity { get; set; }
+        public float Pressure { get; set; }
+
+        public WeatherData(){ }
+
+        public void MeasurementChanged()
+        {
+            
+        }
+
+        public void SetMeasurements(float temperature, float humidity, float pressure)
+        {
+            this.temperature = temperature;
+            this.humidity = humidity;
+            this.pressure = pressure;
+            MeasurementChanged();
+        }
+    }
+}
